81c41c0e4dceba46b1da61b9457b6d1b7a39bea4
SpaceOcean <komsomolenko11@gmail.com>
2018-09-29 20:54:05 +0300

    Исправлено часть ошибок в оформлении caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -9,22 +9,21 @@ def encrypt_caesar(plaintext):
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext = []
-    for i in range (0,len(plaintext)):
-        if (ord(plaintext[i]) > 87 and ord(plaintext[i]) < 91) or (ord(plaintext[i]) > 119 and ord(plaintext[i]) < 123): # для сиволов x,y,z,X,Y,Z
-            ciphertext.append(chr(ord(plaintext[i])-23))
-        elif ((ord(plaintext[i]) > 1068 and ord(plaintext[i]) < 1072) or (ord(plaintext[i]) > 1100 and ord(plaintext[i]) < 1104)) or (ord(plaintext[i]) > 220 and ord(plaintext[i]) < 224) or (ord(plaintext[i]) > 252 and ord(plaintext[i]) < 256): # для сиволов э,ю,я,Э,Ю,Я
-            ciphertext.append(chr(ord(plaintext[i])-29))
-        elif (ord(plaintext[i]) > 64 and ord(plaintext[i]) < 88) or (ord(plaintext[i]) > 96 and ord(plaintext[i]) < 120) or (ord(plaintext[i]) > 191 and ord(plaintext[i]) < 221) or (ord(plaintext[i]) > 223 and ord(plaintext[i]) < 253) or (ord(plaintext[i]) > 1039 and ord(plaintext[i]) < 1068) or (ord(plaintext[i]) > 1071 and ord(plaintext[i]) < 1101):
-            ciphertext.append(chr(ord(plaintext[i])+3))
+    ciphertext = ""
+
+    for i in range (len(plaintext)):
+        elif ('x' <= plaintext[i] <= 'z') or ('X' <= plaintext[i] <= 'Z'): 
+            ciphertext += chr(ord(plaintext[i])-23)
+        elif ('э' <= plaintext[i] <= 'я') or ('Э' <= plaintext[i] <= 'Я'):
+            ciphertext += chr(ord(plaintext[i])-29)
+        elif (('A' >= plaintext[i] < 'X') or ('a' >= plaintext[i] < 'x')):
+            ciphertext += chr(ord(plaintext[i])+3)
+        elif ('А' >= plaintext[i] < 'Э') or ('а' >= plaintext[i] < 'э'):
+            ciphertext += chr(ord(plaintext[i])+3)  
         else:
-            ciphertext.append(plaintext[i])
-    print (''.join(ciphertext))   
-
+            ciphertext += plaintext[i] 
+            
     return ciphertext
-
-encrypt_caesar(str(input ("Введите строку, которую следует закодировать: ")))
-
 
 def decrypt_caesar(ciphertext):
     """
@@ -37,18 +36,24 @@ def decrypt_caesar(ciphertext):
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = []
-    for i in range (0,len(ciphertext)):
-        if (ord(ciphertext[i]) > 64 and ord(ciphertext[i]) < 68) or (ord(ciphertext[i]) > 96 and ord(ciphertext[i]) < 100): # для сиволов a,b,c,A,B,C
-            plaintext.append(chr(ord(ciphertext[i])+23))
-        elif ((ord(ciphertext[i]) > 1038 and ord(ciphertext[i]) < 1043) or (ord(ciphertext[i]) > 1071 and ord(ciphertext[i]) < 1075)) or (ord(ciphertext[i]) > 162 and ord(ciphertext[i]) < 166) or (ord(ciphertext[i]) > 194 and ord(ciphertext[i]) < 198): # для сиволов а,б,в,А,Б,В
-            plaintext.append(chr(ord(ciphertext[i])+29))
-        elif (ord(ciphertext[i]) > 67 and ord(ciphertext[i]) < 91) or (ord(ciphertext[i]) > 99 and ord(ciphertext[i]) < 123) or (ord(ciphertext[i]) > 194 and ord(ciphertext[i]) < 224) or (ord(ciphertext[i]) > 226 and ord(ciphertext[i]) < 256) or (ord(ciphertext[i]) > 1042 and ord(ciphertext[i]) < 1071) or (ord(ciphertext[i]) > 1074 and ord(ciphertext[i]) < 1104):
-            plaintext.append(chr(ord(ciphertext[i])-3))
+    plaintext = ""
+
+    for i in range (len(ciphertext)):
+        if ('A' <= ciphertext[i] <= 'C') or ('a' <= ciphertext[i] <= 'c'): 
+            plaintext += chr(ord(ciphertext[i])+23)
+        elif ('А' <= ciphertext[i] <= 'В') or ('а' <= ciphertext[i] <= 'в'):
+            plaintext += chr(ord(ciphertext[i])+29)
+        elif ('C' > ciphertext[i] <='Z') or ('c' > ciphertext[i] <='z'):
+            plaintext += chr(ord(ciphertext[i])-3)
+        elif ('В' > ciphertext[i] <='Я') or ('в' > ciphertext[i] <='я'):
+            plaintext += chr(ord(ciphertext[i])-3)
         else:
-            plaintext.append(ciphertext[i])
-    print (''.join(plaintext))
+            plaintext += ciphertext[i]
 
     return plaintext
 
-decrypt_caesar(str(input ("Введите строку, которую следует декодировать: ")))
+if __name__ == "__main__":
+    ciphertext = encrypt_caesar(input ("Введите строку, которую следует закодировать: "))
+    print(ciphertext)
+    plaintext = decrypt_caesar(ciphertext)
+    print(plaintext)
