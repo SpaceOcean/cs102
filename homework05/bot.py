3814c8eb5cbc10611c450c291f559beecb979c92
SpaceOcean <komsomolenko11@gmail.com>
2018-12-13 14:53:03 +0300

    Добавлен /near

--- homework05/bot.py
+++ homework05/bot.py
@@ -3,11 +3,14 @@ import config
 import telebot
 import datetime
 from bs4 import BeautifulSoup
+from typing import Optional, Tuple
 
 bot = telebot.TeleBot(config.access_token)
 week_list = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 week_list_rus = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
     'Пятница', 'Суббота', 'Воскресенье']
+
+
 def get_page(group, week=''):
     if week:
         week = str(week) + '/'
@@ -20,8 +23,7 @@ def get_page(group, week=''):
     return web_page
 
 
-def parse_schedule(web_page, day):
-
+def parse_schedule(web_page: list, day: str) -> Optional[Tuple[list, list, list, list]]:
     soup = BeautifulSoup(web_page, "html5lib")
     date = ''
     if day in week_list:
@@ -44,13 +46,15 @@ def parse_schedule(web_page, day):
     # Название дисциплин и имена преподавателей
     lessons_list = schedule_table.find_all("td", attrs={"class": "lesson"})
     lessons_list = [lesson.text.split('\n\n') for lesson in lessons_list]
-    lessons_list = [', '.join([info for info in lesson_info if info]) for lesson_info in lessons_list]
+    lessons_list = [', '.join([info for info in lesson_info if info]) for
+                lesson_info in lessons_list]
 
     return times_list, locations, rooms, lessons_list
 
 
-@bot.message_handler(commands=['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
-def get_schedule(message):
+@bot.message_handler(commands=['monday', 'tuesday', 'wednesday', 'thursday',
+                'friday', 'saturday', 'sunday'])
+def get_schedule(message: telebot.types.Message) -> telebot.types.Message:
     """ Получить расписание на указанный день """
     day, week, group = message.text.split()
     web_page = get_page(group, week)
@@ -60,20 +64,79 @@ def get_schedule(message):
     else:
         times_lst, locations_lst, rooms_lst, lessons_lst = schedule
         resp = ''
-        for time, location, room, lession in zip(times_lst, locations_lst, rooms_lst, lessons_lst):
+        for time, location, room, lession in zip(times_lst, locations_lst, rooms_lst,
+                        lessons_lst):
             resp += '<b>{}</b>, {}, {}, {}\n'.format(time, location, room, lession)
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
 
 @bot.message_handler(commands=['near'])
-def get_near_lesson(message):
+def get_near_lesson(message: telebot.types.Message) -> telebot.types.Message:
     """ Получить ближайшее занятие """
-    # PUT YOUR CODE HERE
-    pass
+    _, group = message.text.split()
+    today = int(datetime.datetime.today().weekday())
+    week = (int(datetime.datetime.now().isocalendar()[1]) + 1) % 2
+    web_page = get_page(group, week)
+    schedule = parse_schedule(web_page, week_list[today])
+    resp = ''
+    flag = 0
+    if schedule is None:
+        while flag == 0:
+            if today == 6:
+                today = 0
+                week = (week + 1) % 2
+            else:
+                today += 1
+            web_page = get_page(group, week)
+            next_schedule = parse_schedule(web_page, week_list[today])
+            if next_schedule is not None:
+                time, location, room, lession = next_schedule
+                resp += '\n' + '\n' + "<b>{}:</b>".format(week_list_rus[today]) + '\n' + '\n'
+                resp += '<b>{}</b>, {}, {}, {}\n'.format(time[0], location[0], room[0], lession[0])
+                flag = 1
+    else:
+        times_lst, locations_lst, rooms_lst, lessons_lst = schedule
+        minute_now = datetime.datetime.now().minute
+        hour_now = datetime.datetime.now().hour
+        for time, location, room, lession in zip(times_lst, locations_lst, rooms_lst, lessons_lst):
+            beginning, _ = time.split('-')
+            hour, minute = beginning.split(':')
+            if hour[0] == '0':
+                hour_int = int(hour[1])
+            else:
+                hour_int = int(hour[0]) * 10 + int(hour[1])
+            if minute[0] == '0':
+                minute_int = int(minute[1])
+            else:
+                minute_int = int(minute[0]) * 10 + int(minute[1])
+            if hour_int == hour_now:
+                if minute_int >= minute_now:
+                    resp += '\n' + '\n' + "<b>{}:</b>".format(week_list_rus[today]) + '\n' + '\n'
+                    resp += '<b>{}</b>, {}, {}, {}\n'.format(time, location, room, lession)
+                    break
+            elif hour_int > hour_now:
+                resp += '\n' + '\n' + "<b>{}:</b>".format(week_list_rus[today]) + '\n' + '\n'
+                resp += '<b>{}</b>, {}, {}, {}\n'.format(time, location, room, lession)
+                break
+        if resp == '':
+            while flag == 0:
+                if today == 6:
+                    today = 0
+                    week = (week + 1) % 2
+                else:
+                    today += 1
+                web_page = get_page(group, week)
+                next_schedule = parse_schedule(web_page, week_list[today])
+                if next_schedule is not None:
+                    time, location, room, lession = next_schedule
+                    resp += '\n' + '\n' + "<b>{}:</b>".format(week_list_rus[today]) + '\n' + '\n'
+                    resp += '<b>{}</b>, {}, {}, {}\n'.format(time[0], location[0], room[0], lession[0])
+                    flag = 1
+    bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
 
 @bot.message_handler(commands=['tommorow'])
-def get_tommorow(message):
+def get_tommorow(message: telebot.types.Message) -> telebot.types.Message:
     """ Получить расписание на следующий день """
     _, group = message.text.split()
     today = int(datetime.datetime.today().weekday())
@@ -82,7 +145,7 @@ def get_tommorow(message):
         today = 0
     else:
         today += 1
-        week = (int(datetime.datetime.now().isocalendar()[1]) + 1) % 2 
+        week = (int(datetime.datetime.now().isocalendar()[1]) + 1) % 2
     web_page = get_page(group, week)
     schedule = parse_schedule(web_page, week_list[today])
     if schedule is None:
@@ -93,11 +156,10 @@ def get_tommorow(message):
         for time, location, room, lession in zip(times_lst, locations_lst, rooms_lst, lessons_lst):
             resp += '<b>{}</b>, {}, {}, {}\n'.format(time, location, room, lession)
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
-    pass
 
 
 @bot.message_handler(commands=['all'])
-def get_all_schedule(message):
+def get_all_schedule(message: telebot.types.Message) -> telebot.types.Message:
     """ Получить расписание на всю неделю для указанной группы """
     _, week, group = message.text.split()
     web_page = get_page(group, week)
@@ -108,11 +170,11 @@ def get_all_schedule(message):
             resp += 'В этот день занятий нет!'
         else:
             times_lst, locations_lst, rooms_lst, lessons_lst = schedule
-            for time, location, room, lession in zip(times_lst, locations_lst, rooms_lst, lessons_lst):
+            for time, location, room, lession in zip(times_lst, locations_lst,
+                        rooms_lst, lessons_lst):
                 resp += '<b>{}</b> {} {} {}\n'.format(time, location, room, lession)
         bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
-
